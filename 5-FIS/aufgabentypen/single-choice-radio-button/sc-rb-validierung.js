28d5a941c64d65e35ccc731ca5bd7b13bc4589f2
kmkrueger <kruegerk@uni-hildesheim.de>
2022-03-22 17:53:37 +0100

    feat: Anpassung Validierung

--- 5-FIS/aufgabentypen/single-choice-radio-button/sc-rb-validierung.js
+++ 5-FIS/aufgabentypen/single-choice-radio-button/sc-rb-validierung.js
@@ -1,14 +1,14 @@
-/* Validierung Single Choice Radio */
+/* Validierung Single Choice Radio Button */
+
 function radioVal() {
-    jQuery("input[type='radio']").parent().removeClass("correct");
-    jQuery("input.solution:checked").parent().addClass("correct");
     jQuery("#alertBox").removeClass("padding bg-success bg-danger");
+    jQuery("input.solution:checked").parent().addClass("correct");
     if(jQuery("input.solution:checked").parent().hasClass("correct")) {
-            jQuery("#alertBox").addClass("padding bg-success rounded p-1 justify-content-center text-center");
+            jQuery("#alertBox").addClass("padding bg-success rounded p-1 text-center");
             jQuery("#alertBox").html("Stimmt!");
     }
     else {
-        jQuery("#alertBox").addClass("padding bg-danger rounded p-1 justify-content-center text-center");
+        jQuery("#alertBox").addClass("padding bg-danger rounded p-1 text-center");
         jQuery("#alertBox").html("Stimmt leider nicht.");
     }
 }
