b7ffff98c38452a37fd67dc69c4dc2d0bd8f0e68
alinaH2022 <hartnack@uni-hildesheim.de>
2022-03-27 17:39:48 +0200

    feat: add Aufgabe s16

---
+++ 2-IR1/js/s16.js
@@ -0,0 +1,21 @@
+function validate(event) {
+  console.log(document.getElementById('antwort3').checked);
+
+
+   if (document.getElementById('antwort3').checked){
+    var paragraph = "<p>";
+    paragraph += "Die Antwort ist korrekt!";
+    paragraph += "</p>";
+    document.getElementById('rueckmeldung').innerHTML = paragraph;
+   } else {
+    var paragraph = "<p>";
+    paragraph += "Die Antwort ist leider falsch!";
+    paragraph += "</p>";
+    document.getElementById('rueckmeldung').innerHTML = paragraph;
+   }
+  
+}
+
+
+//event.target.checked
+//event.target.id
